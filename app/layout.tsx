b7f22ec182899d268167c3fa6a6bf15467edf499
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-02-27 19:56:55 -0300

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,9 +64,7 @@ export const metadata: Metadata = {
   },
   icons: {
   icon: [
-    { url: "/favicon.ico" },
-    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
   ],
   apple: "/apple-touch-icon.png",
 },
